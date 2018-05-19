93b04eac7c6cc0a2f8bfddcce7e79971328a39c2
Alex MacKenzie <alexbcmac7@gmail.com>
2018-05-19 14:30:07 -0700

    Get messages from api end point

--- src/app/client/components/Messages.react.js
+++ src/app/client/components/Messages.react.js
@@ -1,11 +1,36 @@
 import * as React from 'react';
+import axios from 'axios';
+import Message from './Message.react'
 
 class Messages extends React.Component {
+    constructor () {
+        super();
+        this.state = {
+            messages: []
+        }
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick () {
+        axios.get('https://alex-test-autotelic.herokuapp.com/messages/')
+                .then((response) => {
+                    this.setState({messages: response.data.results})
+                })
+    }
+
     render() {
         return (
             <div>
                 Messages
-                {this.props.children}
+                <button onClick={this.handleClick}>Get Messages</button>
+                <ul>
+                    {this.state.messages.map(function (message) {
+                        return (
+                            <Message key={message.id} data={message} />
+                        );
+                    })}
+                </ul>
             </div>
         )
     }
