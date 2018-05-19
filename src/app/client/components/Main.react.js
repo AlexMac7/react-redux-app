36e247ad4116dc1a3933ac8e76e7fa68310a65ab
Alex MacKenzie <alexbcmac7@gmail.com>
2018-05-19 14:47:11 -0700

    Refactor main page to show messages on mount

--- src/app/client/components/Main.react.js
+++ src/app/client/components/Main.react.js
@@ -1,10 +1,34 @@
 import * as React from 'react';
+import axios from "axios/index";
+import Message from './Message.react'
 
 class Main extends React.Component {
+    constructor () {
+        super();
+        this.state = {
+            messages: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get('https://alex-test-autotelic.herokuapp.com/messages/')
+            .then((response) => {
+                this.setState({messages: response.data.results})
+            })
+    }
+
     render() {
         return (
             <div>
-                Hello World!
+                Messages
+                <button onClick={this.handleClick}>Get Messages</button>
+                <ul>
+                    {this.state.messages.map(function (message) {
+                        return (
+                            <Message key={message.id} data={message} />
+                        );
+                    })}
+                </ul>
             </div>
         )
     }
