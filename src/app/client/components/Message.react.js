130c6adcd265839ff2df287412ea534746c38015
Alex MacKenzie <alexbcmac7@gmail.com>
2018-05-19 14:42:49 -0700

    Start to show message
    Add button to each message that makes a get request

--- src/app/client/components/Message.react.js
+++ src/app/client/components/Message.react.js
@@ -1,12 +1,31 @@
 import * as React from 'react';
+import axios from "axios/index";
 
 class Message extends React.Component {
+    constructor () {
+        super();
+        this.state = {
+            message: []
+        }
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick () {
+        console.log(`https://alex-test-autotelic.herokuapp.com/messages/${this.props.data.id}`)
+        axios.get(`https://alex-test-autotelic.herokuapp.com/messages/${this.props.data.id}`)
+            .then((response) => {
+                this.setState({message: response.data})
+            })
+    }
+
     render() {
         const { text, created_at } = this.props.data;
         return (
             <li className={'message'}>
                 <p>{created_at}</p>
                 <p>{text}</p>
+                <button onClick={this.handleClick}>Show Message</button>
             </li>
         )
     }
